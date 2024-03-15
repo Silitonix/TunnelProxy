faefd67e4fe5c182723d8d9929b06007ff0d7a01
Silitonix <silitonix@gmail.com>
2024-03-15 08:25:56 +0330

    MIT licence

--- Client/Client.ts
+++ Client/Client.ts
@@ -1,3 +1,7 @@
+import { Conn } from "./Conn";
+import { Socket } from "bun";
+import { TCPSocketListenOptions } from "bun";
+
 interface Props
 {
   hostname?: string;
@@ -8,10 +12,37 @@ class Client
 {
   constructor (props?: Props)
   {
-    Bun.listen({
-      hostname: props?.hostname ?? '127.0.0.1',
-      port: props?.port ?? 9999,
+    const { hostname, port } = props;
+    this.initialize(hostname, port);
+  }
+
+  // ** FORWARDER **
+
+  //  make a server to deliver the
+  //  Packet from clinet-side into
+  //  the server behind firewalls.
+
+  initialize(hostname: string = '127.0.0.1', port: number = 9999): void
+  {
+    const options: TCPSocketListenOptions<Conn> = {
+      hostname: hostname,
+      port: port,
       socket: {}
-    });
+    };
+
+    try { Bun.listen<Conn>(options); }
+    catch (error)
+    {
+      console.error("Create server faild");
+      process.exit(1);
+    }
+  }
+
+  // ** HANDLERS **
+
+  //  Socket function for bun options
+
+  open(socket: Socket<Conn>)
+  {
   }
 }
