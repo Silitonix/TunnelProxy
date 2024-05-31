7af99ead64eebfa176b0f1d56d0374a01e295bf0
Silitonix <silitonix@gmail.com>
2024-05-31 09:15:24 +0330

    document formatting

--- Library/Socket.ts
+++ Library/Socket.ts
@@ -2,5 +2,3 @@ export type SocketClass<Type> = new (...any: any[]) => Type;
 export interface ISocket {
   listen(hostname: string, port: number): void;
 }
-
-
--- Protocols/Socks5.ts
+++ Protocols/Socks5.ts
@@ -19,6 +19,6 @@ export class Socks extends Tunnel {
   }
 
   //#region Statics
-  
+
   ////#endregion
 }
